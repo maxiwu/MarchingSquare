a6c9b5dfa8be37ea63f09f375e779b2f8777a8a2
maxi <maxi0361@hotmail.com>
2020-06-01 23:53:33 +0800

    first commit

---
+++ Assets/VoxelGrid.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class VoxelGrid : MonoBehaviour {
+
+    //[SerializeField]public int resolutionX;
+    //[SerializeField]public int resolutionY;
+    [SerializeField] public int resolution;
+    private float voxelSize;
+    private bool[] voxels;
+    [SerializeField] private GameObject voxelPrefab;
+
+    private Color colorStart;
+    private Color colorEnd;
+
+    private void Awake()
+    {
+        voxelSize = 1f / resolution;
+        //Initialize(resolution, voxelSize);
+        Debug.LogFormat("grid rez:{0} gridSize:{1}", resolution, voxelSize);
+    }
+
+    public void Initialize(int resolution, float voxelSize)
+    {
+        colorStart = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        colorEnd = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        //voxels = new bool[resolutionX * resolutionY];
+        //voxelSize = 1f / resolution;
+        //Debug.Log("voxel size " + voxelSize);
+        voxels = new bool[resolution * resolution];
+
+        //create voxel
+        for (int i = 0, y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++, i++)
+            {                
+                CreateVoxel(i, x, y);
+            }
+        }
+    }
+
+    private void CreateVoxel(int i, int x, int y)
+    {        
+        GameObject o = Instantiate(voxelPrefab) as GameObject;
+        o.transform.parent = transform;
+        o.transform.localPosition = new Vector3((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
+        
+        o.transform.localScale = Vector3.one * voxelSize;        
+        changeColor(o);
+        //Debug.Log("create voxel at " + o.transform.localPosition);
+    }
+
+    private GameObject changeColor(GameObject pf)
+    {
+        var cubeRenderer = pf.GetComponent<Renderer>();
+
+        //Debug.Log(cubeRenderer == null ? "no render" : "yes render");
+
+        if (cubeRenderer != null)
+        {
+            
+            //float duration = 1.0f;
+            float duration = Random.RandomRange(0f,1f);
+
+            //Call SetColor using the shader property name "_Color" and setting the color to red
+            //cubeRenderer.material.SetColor("_Color", Color.red);
+            //float lerp = Mathf.PingPong(Time.time, duration) / duration;
+            cubeRenderer.material.color = Color.Lerp(colorStart, colorEnd, duration);
+        }
+        return pf;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
---
+++ Assets/VoxelMap.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelMap : MonoBehaviour {
+
+    public float size = 2f;
+
+    public int voxelResolution = 8;
+    public int chunkResolution = 2;
+
+    public VoxelGrid voxelGridPrefab;
+
+    private VoxelGrid[] chunks;
+
+    private float chunkSize, voxelSize, halfSize;
+
+    private void Awake()
+    {
+        halfSize = size * 0.5f;
+        chunkSize = size / chunkResolution;
+        voxelSize = chunkSize / voxelResolution;
+
+        chunks = new VoxelGrid[chunkResolution * chunkResolution];
+        for (int i = 0, y = 0; y < chunkResolution; y++)
+        {
+            for (int x = 0; x < chunkResolution; x++, i++)
+            {
+                Debug.LogFormat("Chunk {0} at x:{1} y:{2}", i, x, y);
+                CreateChunk(i, x, y);
+            }
+        }
+
+        Debug.LogFormat("voxel rez:{0} voxelSize:{1}", voxelResolution, voxelSize);
+    }
+
+    private void CreateChunk(int i, float x, float y)
+    {
+        VoxelGrid chunk = Instantiate(voxelGridPrefab) as VoxelGrid;
+        
+        chunk.Initialize(voxelResolution, voxelSize);
+        chunk.transform.parent = transform;
+        chunk.transform.localPosition = new Vector3(x * chunkSize - halfSize, y * chunkSize - halfSize);
+        //Debug.Log(chunk.transform.localPosition);
+        chunks[i] = chunk;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
